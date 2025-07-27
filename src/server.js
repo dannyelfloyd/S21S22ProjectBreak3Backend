4ea6bbcbbe6de471752cdcd9fb89246135834c0c
dannyelfloyd <dannyelfloyd@gmail.com>
2025-07-27 11:19:31 +0200

    CORS implementation

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middlewares/rateLimiter.js";
 
 dotenv.config();
@@ -12,6 +14,9 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.json());
 app.use(rateLimiter);
+app.use(cors(
+    {origin: "http://localhost:5173"},
+));
 app.use("/api/notes", notesRoutes);
 
 connectDB().then(()=>{
